869b5c38ff4e2ff1a53127f0ada9e7235c155ee8
JuanCastellanos89 <113217035+JuanCastellanos89@users.noreply.github.com>
2022-10-05 18:03:11 -0500

    axios

--- src/helpers/axios-config.js
+++ src/helpers/axios-config.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:4000/'
+    //baseURL: 'http://localhost:4000/'
+    baseURL: 'https://inventarios-backend-jcc.herokuapp.com/'
 })
 export {
     axiosInstance
